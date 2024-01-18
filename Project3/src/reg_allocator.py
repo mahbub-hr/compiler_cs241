61e3ac8fead8c7ebe8e2516e79018f5637983b89
mahbub <mahbub.mmhr@gmail.com>
2024-01-18 10:52:57 -0800

    Project 3 initialization

---
+++ Project3/src/reg_allocator.py
@@ -0,0 +1,22 @@
+class register_allocator:
+
+    def __init__(self):
+        self.reg_array= [0]*30
+
+    '''
+        Find and return the register number of 
+        an emtpy register
+    '''
+    def walk(self):
+        for i in range(1, 30):
+            if self.reg_array[i] == 0:
+                return i
+
+    def allocate_reg(self):
+        reg_no = self.walk()
+        self.reg_array[reg_no] = 1
+        return reg_no
+
+    def deallocate_reg(self, reg_no):
+        self.reg_array[reg_no] = 0
+        return
